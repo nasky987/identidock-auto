e0ba7f279f4b6475779b45e4b942525f30b97cdb
JOHN LEE <nasky987@gmail.com>
2017-03-05 21:50:10 +0900

    jenkins 빌드시 파일 못 찾는 문제로 인한 경로 수정

--- cmd.sh
+++ cmd.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ "$ENV" = 'DEV' ]; then
     echo "Running Devleopment Server"
-    exec python "identidock.py"
+    exec python "app/identidock.py"
 elif [ "$ENV" = 'UNIT' ]; then
     echo "Running Unit Tests"
-    exec python "tests.py"
+    exec python "app/tests.py"
 else
     echo "Running Production Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0.:9191
