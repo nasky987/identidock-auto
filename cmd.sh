9ad82a5e8bccb316a87ed0c6558a19f7ed55add6
JOHN LEE <nasky987@gmail.com>
2017-03-05 22:23:13 +0900

    rollback

--- cmd.sh
+++ cmd.sh
@@ -6,7 +6,7 @@ if [ "$ENV" = 'DEV' ]; then
     exec python "identidock.py"
 elif [ "$ENV" = 'UNIT' ]; then
     echo "Running Unit Tests"
-    exec python "tests.py"
+    exec python "app/tests.py"
 else
     echo "Running Production Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0.:9191
